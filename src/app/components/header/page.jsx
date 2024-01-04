f9cf6ad5b127795cb3dc5be7308c436f7fb0a5ae
rbravo47 <rbravo47@gmail.com>
2024-01-04 17:41:14 +0100

    se modifica la funcion header

--- src/app/components/header/page.jsx
+++ src/app/components/header/page.jsx
@@ -1,12 +1,11 @@
 "use client";
-
 import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import Link from 'next/link';
 import styles from './page.module.css';
 
 const Header = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   return <HeaderClient isMobile={isMobile} />;
 };
 
@@ -28,9 +27,9 @@ const HeaderClient = ({ isMobile }) => {
           <Link href="/doctores">Doctores</Link>
         </nav>
       )}
-      {isOpen && (
+      {isOpen && isMobile && (
         <main className={styles.main}>
-          <nav className={styles.nav}>
+          <nav className={`${styles.nav} ${styles.menu}`}>
             <Link href="/contacto">Contacto</Link>
             <Link href="/conoce_a">Conoce-a</Link>
             <Link href="/doctores">Doctores</Link>
@@ -38,15 +37,15 @@ const HeaderClient = ({ isMobile }) => {
         </main>
       )}
 
-      <img className={styles.logo} src="../../logos/logo.png" alt="Logo" />
+      <img className={styles.logo} src="./../logos/logo.png" alt="Logo" />
       <nav className={styles.nav}>
         <Link href="./../buscar">
           <img src="./icons/material-symbols-light_search.svg" alt="icon search" />Buscar
         </Link>
-        <Link href="https://www.facebook.com/"><img src="../../icons/facebook.svg" alt="Facebook" target="_blank"/></Link>
-        <Link href="https://www.X.com/"><img src="../../icons/simple-icons_x.svg" alt="twitter" target="_blank"/></Link>
-        <Link href="https://www.instagram.com/"><img src="../../icons/mdi_instagram.svg" alt="Instagram" target="_blank"/></Link>
-        <Link href="https://www.tiktok.com/"><img src="../../icons/ic_baseline-tiktok.svg" alt="Tiktok" target="_blank"/></Link>
+        <Link href="https://www.facebook.com/"><img src="./../icons/facebook.svg" alt="Facebook" target="_blank"/></Link>
+        <Link href="https://www.X.com/"><img src="./../icons/simple-icons_x.svg" alt="twitter" target="_blank"/></Link>
+        <Link href="https://www.instagram.com/"><img src="./../icons/mdi_instagram.svg" alt="Instagram" target="_blank"/></Link>
+        <Link href="https://www.tiktok.com/"><img src="./../icons/ic_baseline-tiktok.svg" alt="Tiktok" target="_blank"/></Link>
       </nav>
     </header>
   );
